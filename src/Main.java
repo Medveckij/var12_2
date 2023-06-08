272066da399aa8db69354a299483fd5a44204a4d
Max <medveckijmaksim777@gmail.com>
2023-06-08 17:42:09 +0300

    jsxsbjh

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("опставте 20 будь ласка");
+        System.out.println("поставте 20 будь ласка");
+        System.out.println("jdsbhjfbjohs");
     }
 }
